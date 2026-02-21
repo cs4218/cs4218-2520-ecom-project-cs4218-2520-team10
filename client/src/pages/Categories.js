40d9b971793580736b8a77bd107a2cf48eaa8b91
Shaun Lee <shaunlxw@gmail.com>
2026-02-22 00:27:26 +0800

    Add unit tests for pages/Categories
    Summary:
    - 4 tests added
    - 1 bug found
    - 1 bug fixed

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
 import Layout from "../components/Layout";
+
 const Categories = () => {
   const { categories } = useCategory();
   return (
     <Layout title={"All Categories"}>
       <div className="container">
         <div className="row">
-          {categories.map((c) => (
+          {categories?.map((c) => ( // Fix: guard against categories being undefined or null - Shaun Lee Xuan Wei A0252626E
             <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
               <Link to={`/category/${c.slug}`} className="btn btn-primary">
                 {c.name}
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,19 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js", "<rootDir>/client/src/hooks/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/hooks/*.test.js",
+    "<rootDir>/client/src/pages/Categories.test.js"
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/hooks/**"],
+  collectCoverageFrom: [
+    "client/src/pages/Auth/**",
+    "client/src/hooks/**",
+    "client/src/pages/Categories.js"
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
