0bcc48cc0f8ae123c25482aa9e46a5dcae9fd0b4
bertrandong <bertrandongchangheng@gmail.com>
2026-03-18 01:58:42 +0800

    chore: revert console log line

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ connectDB();
 
 const PORT = process.env.PORT || 6060;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
 });
