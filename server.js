41e2ab9fd9cdbaa28ef04c9b9cc77343d6eac8fe
bertrandong <99965015+bertrandong@users.noreply.github.com>
2026-03-18 17:24:24 +0800

    Initialize app for integration and UI testing (#34)
    * chore: add relevant packages for integration and unit testing, break down server component for testing purposes
    
    * chore: revert console log line

--- server.js
+++ server.js
@@ -1,82 +1,12 @@
-import express from "express";
-import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
+import app from './app.js';
 import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
+import dotenv from "dotenv";
 
-// Enhancement: Add Swagger documentation for to improve developer experience and facilitate API testing - YAN WEIDONG A0258151H
-// Swagger definition
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0', // Specify the OpenAPI version
-    info: {
-      title: 'E-commerce API',
-      version: '1.0.0',
-      description: 'API documentation for the E-commerce application',
-    },
-    servers: [
-      {
-        url: `http://localhost:${process.env.PORT || 6060}`, // Base URL for your API
-      },
-    ],
-    components: {
-      securitySchemes: {
-        Authorization: {
-          type: "apiKey",
-          in: "header",
-          name: "authorization",
-          description: 'Enter JWT Bearer token **_only_**',
-        },
-      },
-    },
-    security: [
-      {
-        Authorization: [],
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // Path to the API docs
-};
-
-const specs = swaggerJsdoc(swaggerOptions);
-console.log(specs);
-
-// configure env
 dotenv.config();
 
-//database config
 connectDB();
 
-const app = express();
-
-//middlewares
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
-// Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-
-//routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
-
-// rest api
-
-app.get('/', (req,res) => {
-    res.send("<h1>Welcome to ecommerce app</h1>");
-});
-
 const PORT = process.env.PORT || 6060;
-
 app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
+  console.log(`Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
 });
